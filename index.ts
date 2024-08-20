72cf68f3658a7838b7f7f9c1f0407a09f7fa8e2f
Umar Ghafoor <164578000+umarghafoor420@users.noreply.github.com>
2024-08-21 03:02:03 +0500

    Add files via upload
    This is a Currency  Converter You can Convert  5 to 6 Currency

---
+++ index.ts
@@ -0,0 +1,32 @@
+import inquirer from 'inquirer';
+const Currency:any={
+    USD:1,
+    EUR:0.91,
+    GBP:0.76,
+    INR:74.57,
+    PKR:280
+};
+let user_answer:any= await inquirer.prompt([{
+    name:"from",
+    message:"Enter From Currency",
+    type:"list",
+    choices:["USD","EUR","GBP","IND","PKR"]
+},
+{name:"TO",
+message:"Enter To Currency",
+type:'list',
+choices:["USD","EUR","GBP","IND","PKR"]
+},
+{
+    name:"Amount",
+    message:"Enter Your Amount",
+    type:"number"},
+]
+)
+
+let FromAmount =Currency[user_answer.from]
+let ToAmount=Currency[user_answer.TO]
+let Amount=user_answer.Amount
+let baseAmount=Amount/FromAmount
+let ConvertedAmount=baseAmount*ToAmount
+console.log(ConvertedAmount); 
